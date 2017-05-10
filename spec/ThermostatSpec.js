df1c189a780fab469b74c73033cce98448f1ac17
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-10 14:55:11 +0100

    Added off method for powertsaving

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,5 +9,38 @@ describe("Thermostat", function(){
     expect(thermostat).toBeDefined();
   });
 
+  it("Starts at 20 degrees", function(){
+    expect(thermostat.temp).toEqual(20);
+  });
 
-}); 
+  it("Can increase in temp", function(){
+    thermostat.increase()
+    expect(thermostat.temp).toEqual(21);
+  });
+
+  it("Can decrease in temp", function(){
+    thermostat.decrease()
+    expect(thermostat.temp).toEqual(19);
+  });
+
+  it("Does not decrease below 10 degrees", function(){
+    thermostat.temp = 10
+    thermostat.decrease()
+    expect(thermostat.temp).toEqual(10);
+  });
+
+  it("Is in powersaving mode by default" , function(){
+    expect(thermostat.isPowersaving).toEqual(true)
+  })
+
+  it("Turns Powersaving mode off", function(){
+    thermostat.off()
+    expect(thermostat.isPowersaving).toEqual(false)
+  });
+
+  it("Has a max temp of 25 if in powersaving mode",function(){
+  thermostat.temp = 25
+  thermostat.increase()
+  expect(thermostat.temp).toEqual(25);
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,32 @@
 function Thermostat() {
-
+  this.temp = 20
+  this.isPowersaving = true
+  this.maxTemp = 25
+  this.minTemp = 10
 }
+
+Thermostat.prototype.increase = function() {
+  if (this.temp === this.maxTemp){
+    console.log("Maximum Temperature Reached")
+  } else {
+    this.temp++
+  }
+}
+
+Thermostat.prototype.decrease = function() {
+  if (this.temp === this.minTemp){
+    console.log("Minimum Temperature Reached")
+  } else {
+    this.temp--
+  }
+}
+
+Thermostat.prototype.off = function() {
+  this.isPowersaving = false;
+  this.maxTemp = 32;
+}
+
+var thermostat = new Thermostat()
+thermostat.off()
+console.log(thermostat.isPowersaving)
+console.log(thermostat.maxTemp)
